93f4b90559af15cfcf3c5aae818e7f375fcbe4b9
Mikolaj Knap <mikozxc@gmail.com>
2023-04-08 19:22:12 +0200

    Added binary search tree functions, Comments added

--- lib_bst.cpp
+++ lib_bst.cpp
@@ -54,6 +54,7 @@ bool BST::leaf(Node* n){
     if(n->getLeft() == nullptr && n->getRight() == nullptr) return true;
     else return false;
 }
+
 Node* BST::left(Node* n){
     if(n == nullptr) throw std::runtime_error("Node can't be null");
     return n->getLeft();
@@ -120,5 +121,125 @@ void BST::clear(Node* n){
     }
 }
 
+
+
+Node* BST::search(int x){
+    Node* temp = root;
+    while(!isEmpty(temp) && retrieve(temp) != x){
+        if(retrieve(temp) > x) temp = left(temp);
+        else temp = right(temp);
+    }
+    return temp;
+}
+
+Node* BST::minimum(Node* n){
+    if(isEmpty(n)) return nullptr;
+    Node* temp = n;
+    while(!isEmpty(left(temp))){
+        temp = left(temp);
+    }
+    return temp;
+}
+
+Node* BST::maximum(Node* n){
+    if(isEmpty(n)) return nullptr;
+    Node* temp = n;
+    while(!isEmpty(right(temp))){
+        temp = right(temp);
+    }
+    return temp;
+}
+
+Node* BST::successor(Node* n){
+    if(isEmpty(n)) return nullptr;
+    else if(!isEmpty(right(n))) return minimum(right(n));
+    else{
+        Node* p = parent(n);
+        while(!isEmpty(p) && n == right(p)){
+            n = p;
+            p = parent(p);
+        }
+        return p;
+    }
+}
+
+Node* BST::predecessor(Node* n){
+    if(isEmpty(n)) return nullptr;
+    else if(!isEmpty(left(n))) return maximum(left(n));
+    else{
+        Node* p = parent(n);
+        while(!isEmpty(p) && n == left(p)){
+            n = p;
+            p = parent(p);
+        }
+        return p;
+    }
+}
+
+void BST::insert(int x){
+    Node* temp = root;
+    if(isEmpty(temp)){
+        Node* newNode = new Node(x,nullptr,nullptr,nullptr);
+        root = newNode;
+    }
+    else{
+        while(1){
+            if(retrieve(temp) > x){
+                if(!isEmpty(left(temp)))
+                    temp = left(temp);
+                else{
+                    Node* newNode = new Node(x,nullptr,nullptr,temp);
+                    temp->setLeft(newNode);
+                    break;
+                }
+            }
+            if(retrieve(temp) < x){
+                if(!isEmpty(right(temp)))
+                    temp = right(temp);
+                else{
+                    Node* newNode = new Node(x,nullptr,nullptr,temp);
+                    temp->setRight(newNode);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void BST::del(Node* n){
+    if(isEmpty(n)) return;
+    else if(leaf(n)){
+        if(isEmpty(n->getParent())){
+            root = nullptr;
+        }
+        else{
+            Node* p = n->getParent();
+            if(n == p->getLeft()) p->setLeft(nullptr);
+            else p->setRight(nullptr);
+        }
+        delete n;
+        return;
+    }
+    else if (!isEmpty(n->getLeft()) && !isEmpty(n->getRight())){
+        Node* s = successor(n);
+        n->setValue(s->getValue());
+        del(s);
+    }
+    else{
+        Node* s;
+        if(!isEmpty(n->getLeft())) s = n->getLeft();
+        else s = n->getRight();
+        Node* p = n->getParent();
+        if(isEmpty(p)){
+            root = s;
+        }
+        else{
+            if(n == p->getLeft()) p->setLeft(s);
+            else p->setRight(s);
+        }
+        s->setParent(p);
+        delete n;
+    }
+}
 
 
--- lib_bst.h
+++ lib_bst.h
@@ -8,6 +8,13 @@ private:
     Node* parent;
     int value;
 public:
+    /**
+    Constructor for Node class
+    @param value Value to bet set in node
+    @param left Pointer to the left child node
+    @param right Pointer to the right child node
+    @param parent Pointer to the parent node
+    */
     Node(int value, Node* left, Node* right, Node* parent);
     Node* getLeft();
     void setLeft(Node* temp);
@@ -23,25 +30,137 @@ class BST{
 private:
     Node* root;
 public:
+    /**
+    Constructor for BST
+    */
     BST();
+
+    /**
+    Checks if node/tree is empty
+    @param n Node/rootOfTree to check
+    @return Returns true if the node/tree is empty
+    */
     bool isEmpty(Node* n);
+
+    /**
+    Checks if node is leaf
+    @param n Node to check
+    @return Returns true if the node is a leaf
+    */
     bool leaf(Node* n);
+
+    /**
+    Gets left child of a node
+    @param n Node to get the left child from
+    @return Returns pointer to the left child
+    */
     Node* left(Node* n);
+
+    /**
+    Gets right child of a node
+    @param n Node to get the right child from
+    @return Returns pointer to the right child
+    */
     Node* right(Node* n);
+
+    /**
+    Gets parent of a node
+    @param n Node to get the parent from
+    @return Returns pointer to the parent
+    */
     Node* parent(Node* n);
-    int retrieve(Node *n);
+
+    /**
+    Retrieves the value of a node
+    @param n Node to retrieve the value from
+    @return Returns the value of the node
+    */
+    int retrieve(Node* n);
+
+    /**
+    Traverse the tree in preorder
+    @param n Root node of the subtree to traverse
+    */
     void preorder(Node* n);
+
+    /**
+    Traverse the tree in inorder
+    @param n Root node of the subtree to traverse
+    */
     void inorder(Node* n);
+
+    /**
+    Traverse the tree in postorder
+    @param n Root node of the subtree to traverse
+    */
     void postorder(Node* n);
+
+    /**
+    Get the size of a tree/subtree
+    @param n Root node of the tree/subtree
+    @return Returns the size of the tree/subtree
+     */
     int size(Node* n);
+
+    /**
+    Get the height of a tree/subtree
+    @param n Root node of the tree/subtree
+    @return Returns the height of the tree/subtree
+    */
     int height(Node* n);
+
+    /**
+    Clears a tree/subtree
+    @param n Root node of the tree/subtree
+    */
     void clear(Node* n);
 
-    Node* search(Node* n, int x);
+    /**
+    Search for a node with a given value
+    @param x The value to search for
+    @return Returns pointer to the node with the value or nullptr if not found
+    */
+    Node* search(int x);
+
+    /**
+    Get the node with the minimum value in the tree/subtree
+    @param n Root node of the tree/subtree
+    @return Returns pointer to the node with the minimum value in the tree/subtree or nullptr if the tree/subtree is empty
+    */
     Node* minimum(Node* n);
+
+    /**
+    Get the node with the maximum value in the tree/subtree
+    @param n Root node of the tree/subtree
+    @return Returns pointer to the node with the maximum value in the tree/subtree or nullptr if the tree/subtree is empty
+    */
     Node* maximum(Node* n);
+
+    /**
+    Get the successor of a given node
+    @param n Node to get the successor of
+    @return Pointer to the successor node or nullptr if there is no successor
+    */
     Node* successor(Node* n);
+
+    /**
+    Get the predecessor of a given node
+    @param n Node to get the predecessor of
+    @return Pointer to the predecessor node or nullptr if there is no predecessor
+    */
     Node* predecessor(Node* n);
+
+    /**
+    Insert a node with a given value 
+    @param x Value of the node to insert
+    */
+    void insert(int x);
+
+    /**
+    Delete a given node from the tree
+    @param n Node to delete
+    */
+    void del(Node* n);
 };
 
 
